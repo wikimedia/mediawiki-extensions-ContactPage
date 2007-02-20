195132074b10f860af8af4adeef18a623e5ff001
Raimond Spekking <raymond@users.mediawiki.org>
2007-02-20 14:30:06 +0000

    * (bug 9039) Update French messages   patch by Rémi Kaupp

---
+++ ContactPage.i18n.fr.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Internationalisation file for the ContactPage extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Daniel Kinzler, brightbyte.de
+ * @copyright © 2007 Daniel Kinzler
+ * @licence GNU General Public Licence 2.0 or later
+ */
+
+$messages['contact']= 'Contact';
+$messages['contactpage']= 'Contact';
+$messages['contactpage-title']= 'Contact';
+$messages['contactpage-pagetext']= 'Veuillez utiliser le formulaire ci-dessous pour nous contacter.';
+$messages['contactpage-defsubject']= 'Message';
+$messages['contactpage-subject-and-sender']= '$1 (de $2)';
+
+$messages['contactpage-fromname']= 'Votre nom*';
+$messages['contactpage-fromaddress']= 'Votre adresse e-mail**';
+
+$messages['contactpage-formfootnotes']= '
+* optionel<br/>
+** optionel mais requis si vous désirez une réponse
+';
+?>
