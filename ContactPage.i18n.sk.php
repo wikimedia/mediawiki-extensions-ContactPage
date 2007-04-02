0b87dfd6f111f3aa1e7150964498912798f696f7
Raimond Spekking <raymond@users.mediawiki.org>
2007-04-02 09:16:38 +0000

    * (bug 8536) Update Slovak extension translations   Patch by helix84

---
+++ ContactPage.i18n.sk.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Internationalisation file for the ContactPage extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Daniel Kinzler, brightbyte.de
+ * @copyright © 2007 Daniel Kinzler, helix84
+ * @licence GNU General Public Licence 2.0 or later
+ */
+
+$messages['contact']= 'Kontaktná stránka';
+$messages['contactpage']= 'Kontaktná stránka';
+$messages['contactpage-title']= 'Kontakt';
+$messages['contactpage-pagetext']= 'Prosím, použite tento formulár, aby ste nás kontaktovali.';
+$messages['contactpage-defsubject']= 'Správa';
+$messages['contactpage-subject-and-sender']= '$1 (od $2)';
+
+$messages['contactpage-fromname']= 'vaše meno *';
+$messages['contactpage-fromaddress']= 'váš email **';
+
+$messages['contactpage-formfootnotes']= '
+* voliteľné<br />
+** voliteľné, ale potrebné ak chcete odpoveď
+';
+?>
