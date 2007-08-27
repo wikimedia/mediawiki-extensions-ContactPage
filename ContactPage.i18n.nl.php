d3a7dad6f343c999484fe55839a0d45f57632651
Raimond Spekking <raymond@users.mediawiki.org>
2007-08-27 07:48:23 +0000

    * (bug 11068) i18n updates/additions Dutch/nld for extensions   Patch by Siebrand

---
+++ ContactPage.i18n.nl.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Internationalisation file for the ContactPage extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Daniel Kinzler, brightbyte.de
+ * @copyright Â© 2007 Daniel Kinzler, Siebrand Mazeland
+ * @licence GNU General Public Licence 2.0 or later
+ */
+
+$messages['contact']= 'ContactPagina';
+$messages['contactpage']= 'ContactPagina';
+$messages['contactpage-title']= 'Contact';
+$messages['contactpage-pagetext']= 'Gebruik alstublieft het onderstaande formulier om contact met ons op te nemen.';
+$messages['contactpage-defsubject']= 'Contactbericht';
+$messages['contactpage-subject-and-sender']= '$1 (van $2)';
+
+$messages['contactpage-fromname']= 'uw naam *';
+$messages['contactpage-fromaddress']= 'uw e-mailadres **';
+
+$messages['contactpage-formfootnotes']= '
+* optioneel<br />
+** optioneel, maar noodzakelijk als antwoord gewenst is';
