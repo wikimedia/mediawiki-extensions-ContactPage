0282935af8b357310ae80b9f39a2987827739fdd
Raimond Spekking <raymond@users.mediawiki.org>
2007-08-30 10:55:14 +0000

    * (bug 11120) Update Chinese extension translations   Patches by Shinjiman

---
+++ ContactPage.i18n.zh-yue.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Internationalisation file for the ContactPage extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Daniel Kinzler, brightbyte.de
+ * @translator Shinjiman
+ * @copyright c 2007 Daniel Kinzler
+ * @licence GNU General Public Licence 2.0 or later
+ */
+
+/* Inherit yue localisation messages */
+require_once('ContactPage.i18n.yue.php');
