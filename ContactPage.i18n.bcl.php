57538f85fd08c1937168d3e0204ff4f9f593e996
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-09-05 21:38:14 +0000

    Localisation updates for MediaWiki extension messages from Betawiki * bcl: 15 extensions updated

---
+++ ContactPage.i18n.bcl.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Internationalisation file for the ContactPage extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Daniel Kinzler, brightbyte.de
+ * @copyright Â© 2007 Daniel Kinzler, Filipinayzd
+ * @licence GNU General Public Licence 2.0 or later
+ */
+
+$messages = array(
+'contactpage-subject-and-sender' => '$1 (poon $2)',
+'contactpage-fromname' => 'pangaran mo *',
+);
