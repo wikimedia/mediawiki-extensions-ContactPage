57abc74639bd3c57335b095d0469d3d03a5e1948
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-09-05 21:49:57 +0000

    Localisation updates for MediaWiki extension messages from Betawiki * hsb: 8 extensions updated

---
+++ ContactPage.i18n.hsb.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Internationalisation file for the ContactPage extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Daniel Kinzler, brightbyte.de
+ * @copyright Â© 2007 Daniel Kinzler, Michawiki and Dundak
+ * @licence GNU General Public Licence 2.0 or later
+ */
+
+$messages = array(
+'contact' => 'Kontaktna strona',
+'contactpage' => 'Kontaktna strona',
+'contactpage-title' => 'Kontakt',
+'contactpage-pagetext' => 'Prošu wužij tutón formular, zo by nas skontaktował.',
+'contactpage-defsubject' => 'Kontaktna zdźělenka',
+'contactpage-subject-and-sender' => '$1 (z $2)',
+'contactpage-fromname' => 'twoje mjeno *',
+'contactpage-fromaddress' => 'twoja e-mejlowa adresa **',
+'contactpage-formfootnotes' => '* opcionalny<br/>
+** opcionalny, je wšak trjeba, zo by so móhła wotmołwa pósłać',
+);
