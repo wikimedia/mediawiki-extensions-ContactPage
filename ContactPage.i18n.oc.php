ee450ebdc15b063f0d4c867968387711d19410fa
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-09-05 21:19:14 +0000

    Localisation updates for MediaWiki extension messages from Betawiki * oc: 12 extensions updated

---
+++ ContactPage.i18n.oc.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Internationalisation file for the ContactPage extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Daniel Kinzler, brightbyte.de
+ * @copyright © 2007 Daniel Kinzler, Cedric31
+ * @licence GNU General Public Licence 2.0 or later
+ */
+
+$messages = array(
+'contact' => 'Per nos contactar',
+'contactpage' => 'Contacte',
+'contactpage-title' => 'Contacte',
+'contactpage-pagetext' => 'Utilizatz lo formulari çaijos per nos contactar.',
+'contactpage-defsubject' => 'Messatge',
+'contactpage-subject-and-sender' => '$1 (de $2)',
+'contactpage-fromname' => 'Vòstre nom*',
+'contactpage-fromaddress' => 'Vòstra adreça electronica**',
+'contactpage-formfootnotes' => '* opcional<br/> ** opcional mas requesit se desiratz una responsa',
+);
