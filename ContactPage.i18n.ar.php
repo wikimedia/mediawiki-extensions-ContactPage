eefbeec7798a9f59d541201e06b94a2dd98f956b
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-09-09 18:47:23 +0000

    Localisation updates from Betawiki. * Fixes and additions to 6 extensions for ar.

---
+++ ContactPage.i18n.ar.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Internationalisation file for the ContactPage extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Daniel Kinzler, brightbyte.de
+ * @copyright Â© 2007 Daniel Kinzler, Filipinayzd
+ * @licence GNU General Public Licence 2.0 or later
+ */
+
+$messages = array(
+'contact' => 'صفحة الاتصال',
+'contactpage' => 'صفحة الاتصال',
+'contactpage-title' => 'اتصل',
+'contactpage-pagetext' => 'من فضلك استخدم الاستمارة بالأسفل للاتصال بنا.',
+'contactpage-defsubject' => 'رسالة الاتصال',
+'contactpage-subject-and-sender' => '$1 (من $2)',
+'contactpage-fromname' => 'اسمك *',
+'contactpage-fromaddress' => 'بريدك الإلكتروني **',
+'contactpage-formfootnotes' => '* اختياري<br/>
+** اختياري لكن ضروري لو أردت إجابة',
+);
