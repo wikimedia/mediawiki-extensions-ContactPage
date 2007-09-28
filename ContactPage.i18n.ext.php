acd55f9eac615b187b898e4d8dcbffcbc508b198
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-09-28 08:59:16 +0000

    Localisation updates from Betawiki. * Fixes and additions to 26 extensions for am, cs, ext, fo, hsb, la.

---
+++ ContactPage.i18n.ext.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Internationalisation file for the ContactPage extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Daniel Kinzler, brightbyte.de
+ * @copyright © 2007 Daniel Kinzler, Betawiki users
+ * @licence GNU General Public Licence 2.0 or later
+ */
+
+$messages = array(
+'contactpage-subject-and-sender' => '$1 (endi $2)',
+'contactpage-fromname' => 'el tu nombri *',
+'contactpage-fromaddress' => 'el tu email **',
+);
