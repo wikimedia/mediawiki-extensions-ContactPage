d55deaff33162dd703c64192683911829d0dd056
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-10-04 18:18:43 +0000

    Localisation updates from Betawiki. * Fixes and additions to 31 extensions for ar, bcl, gl, hsb, sv, tet.

--- ContactPage.i18n.sv.php
+++ ContactPage.i18n.sv.php
@@ -6,12 +6,10 @@
  * @package MediaWiki
  * @subpackage Extensions
  * @author Daniel Kinzler, brightbyte.de
- * @copyright Â© 2007 Daniel Kinzler, Filipinayzd
+ * @copyright © 2007 Daniel Kinzler, Betawiki users
  * @licence GNU General Public Licence 2.0 or later
  */
 
 $messages = array(
-'contactpage-subject-and-sender' => '$1 (poon $2)',
-'contactpage-fromname' => 'pangaran mo *',
-'contactpage-fromaddress' => 'e-surat mo **',
+'contact' => 'Kontakta oss',
 );
