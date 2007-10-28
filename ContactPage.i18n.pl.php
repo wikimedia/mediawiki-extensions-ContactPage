428857edbcde1f5594ac94113db2efc378e60561
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-10-28 23:55:11 +0000

    Localisation updates from Betawiki. * Fixes and additions to 54 extensions for ang, ar, fo, gl, hr, hsb, hy, is, ja, kn, la, nl, no, pl, pms, sk

---
+++ ContactPage.i18n.pl.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Internationalisation file for the ContactPage extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Daniel Kinzler, brightbyte.de
+ * @copyright © 2007 Daniel Kinzler, Betawiki users
+ * @licence GNU General Public Licence 2.0 or later
+ */
+
+$messages = array(
+'contact' => 'StronaKontaktowa',
+'contactpage' => 'StronaKontaktowa',
+'contactpage-title' => 'Kontakt',
+'contactpage-pagetext' => 'Prosimy użyć poniższego formularza by skontaktować się z nami',
+'contactpage-defsubject' => 'Wiadomość',
+'contactpage-subject-and-sender' => '$1 (z $2)',
+'contactpage-fromname' => 'twoje imię i nazwisko *',
+'contactpage-fromaddress' => 'twój e-mail **',
+'contactpage-formfootnotes' => '* opcjonalne<br/> ** opcjonalne, ale wymagane, jeśli chcesz otrzymać odpowiedź',
+'contactpage-captcha-failed' => 'Aby wysłać tę wiadomość, prosimy rozwiązać to zadanie ([[Special:Captcha/help|objaśnienie]])',
+);
---
+++ ContactPage.i18n.pms.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Internationalisation file for the ContactPage extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Daniel Kinzler, brightbyte.de
+ * @copyright © 2007 Daniel Kinzler, Betawiki users
+ * @licence GNU General Public Licence 2.0 or later
+ */
+
+$messages = array(
+'contact' => 'Pàgina ëd contat',
+'contactpage' => 'Pàgina ëd contat',
+'contactpage-title' => 'Contat',
+'contactpage-pagetext' => 'Për piasì, për contatene ch\'a dòvra ël mòdulo ambelessì sota.',
+'contactpage-defsubject' => 'Messagi',
+'contactpage-subject-and-sender' => '$1 (da $2)',
+'contactpage-fromname' => 'sò nòm *',
+'contactpage-fromaddress' => 'soa adrëssa ëd pòsta eletrònica **',
+'contactpage-formfootnotes' => '* opsional<br/> ** opsional, ma për podej avej d\'arspòsta a venta butelo',
+'contactpage-captcha' => 'Për mandé via ël messagi, për piasì ch\'arzòlva ël test antirumenta ([[Special:Captcha/help|pì d\'anformassion]])',
+'contactpage-captcha-failed' => 'Test antirumenta falì! ([[Special:Captcha/help|pì d\'anformassion]])',
+);
