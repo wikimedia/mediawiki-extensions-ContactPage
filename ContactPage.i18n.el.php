829380a2fc599a761c09add5348030b16b89a05b
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-11-15 23:11:49 +0000

    Localisation updates from Betawiki. * Fixes and additions to 12 extensions for el, kaa, la, ss

---
+++ ContactPage.i18n.el.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Internationalisation file for the ContactPage extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Daniel Kinzler, brightbyte.de
+ * @copyright © 2007 Daniel Kinzler, Betawiki users
+ * @licence GNU General Public Licence 2.0 or later
+ */
+
+$messages = array(
+	'contact' => 'Σελίδα επαφών',
+	'contactpage' => 'Σελίδα επαφών',
+	'contactpage-title' => 'Επαφή',
+	'contactpage-subject-and-sender' => '$1 (από $2)',
+	'contactpage-fromname' => 'το όνομα σας *',
+	'contactpage-fromaddress' => 'το ηλεκτρονικό ταχυδρομείο σας **',
+);
