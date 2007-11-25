c78d978ed5a2d9f0b2d63c7e7c21fb857ec87219
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-11-25 15:17:41 +0000

    Localisation updates from Betawiki. * Fixes and additions to 45 extensions for ar, el, fr, frp, grc, hr, hsb, io, ka, nl, pt, stq

--- ContactPage.i18n.fr.php
+++ ContactPage.i18n.fr.php
@@ -11,16 +11,17 @@
  */
 
 $messages = array(
-'contact' => 'Pour nous contacter',
-'contactpage' => 'Contact',
-'contactpage-pagetext' => 'Veuillez utiliser le formulaire ci-dessous pour nous contacter.',
-'contactpage-defsubject' => 'Message',
-'contactpage-subject-and-sender' => '$1 (de $2)',
-'contactpage-fromname' => 'Votre nom*',
-'contactpage-fromaddress' => 'Votre adresse courriel**',
-'contactpage-formfootnotes' => '
+	'contact' => 'Pour nous contacter',
+	'contactpage' => 'Contact',
+	'contactpage-title' => 'Contacter',
+	'contactpage-pagetext' => 'Veuillez utiliser le formulaire ci-dessous pour nous contacter.',
+	'contactpage-defsubject' => 'Message',
+	'contactpage-subject-and-sender' => '$1 (de $2)',
+	'contactpage-fromname' => 'Votre nom*',
+	'contactpage-fromaddress' => 'Votre adresse courriel**',
+	'contactpage-formfootnotes' => '
 * optionel<br/>
 ** optionel mais requis si vous désirez une réponse',
-'contactpage-captcha' => 'Pour envoyer le message, prière de résoudre le captcha ([[Special:Captcha/help|aide]])',
-'contactpage-captcha-failed' => 'Vous n\'avez pas décodé le captcha ! ([[Special:Captcha/help|aide]])',
+	'contactpage-captcha' => 'Pour envoyer le message, prière de résoudre le captcha ([[Special:Captcha/help|aide]])',
+	'contactpage-captcha-failed' => 'Vous n\'avez pas décodé le captcha ! ([[Special:Captcha/help|aide]])',
 );
