501eb15f19d70dfb085386cd79577f2e3d599d9e
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-11-27 09:49:24 +0000

    Localisation updates from Betawiki. * Fixes and additions to 50 extensions for ar, el, et, fr, hr, hsb, io, ka, la, nap, nl, pt, sk, stq, tet, vo

---
+++ ContactPage.i18n.nap.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Internationalisation file for the ContactPage extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Daniel Kinzler, brightbyte.de
+ * @copyright © 2007 Daniel Kinzler, Betawiki users
+ * @licence GNU General Public Licence 2.0 or later
+ */
+
+$messages = array(
+	'contact' => 'Scrìvece',
+);
---
+++ ContactPage.i18n.pt.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Internationalisation file for the ContactPage extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Daniel Kinzler, brightbyte.de
+ * @copyright © 2007 Daniel Kinzler, Betawiki users
+ * @licence GNU General Public Licence 2.0 or later
+ */
+
+$messages = array(
+	'contactpage-defsubject' => 'Mensagem de Contacto',
+	'contactpage-subject-and-sender' => '$1 (de $2)',
+	'contactpage-fromname' => 'o seu nome *',
+	'contactpage-fromaddress' => 'o seu email **',
+	'contactpage-captcha' => 'Para enviar a mensagem, por favor, resolva o \'\'captcha\'\' ([[{{ns:special}}:Captcha/help|mais informação]])',
+);
