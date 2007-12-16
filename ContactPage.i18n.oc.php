cd7115ba4973fb48dcef4cd7f8217035bcba8306
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-12-16 23:54:39 +0000

    Localisation updates from Betawiki (2007-12-17 0:15 CET). * Fixes and additions to 42 extensions for bg, br, el, gl, loz, nl, oc, stq

--- ContactPage.i18n.oc.php
+++ ContactPage.i18n.oc.php
@@ -10,16 +10,19 @@
  * @licence GNU General Public Licence 2.0 or later
  */
 
+/** Occitan (Occitan)
+ * @author Cedric31
+ */
 $messages = array(
-	'contact' => 'Per nos contactar',
-	'contactpage' => 'Contacte',
-	'contactpage-title' => 'Contacte',
-	'contactpage-pagetext' => 'Utilizatz lo formulari çaijos per nos contactar.',
-	'contactpage-defsubject' => 'Messatge',
+	'contact'                        => 'Per nos contactar',
+	'contactpage'                    => 'Contacte',
+	'contactpage-title'              => 'Contacte',
+	'contactpage-pagetext'           => 'Utilizatz lo formulari çaijos per nos contactar.',
+	'contactpage-defsubject'         => 'Messatge',
 	'contactpage-subject-and-sender' => '$1 (de $2)',
-	'contactpage-fromname' => 'Vòstre nom*',
-	'contactpage-fromaddress' => 'Vòstra adreça electronica**',
-	'contactpage-formfootnotes' => '* opcional<br/> ** opcional mas requesit se desiratz una responsa',
-	'contactpage-captcha' => 'Per mandar lo messatge, prec de resoudre lo captcha ([[Special:Captcha/help|ajuda]])',
-	'contactpage-captcha-failed' => 'Avètz pas decodat lo captcha ! ([[Special:Captcha/help|ajuda]])',
+	'contactpage-fromname'           => 'Vòstre nom*',
+	'contactpage-fromaddress'        => 'Vòstra adreça electronica**',
+	'contactpage-formfootnotes'      => '* opcional<br/> ** opcional mas requesit se desiratz una responsa',
+	'contactpage-captcha'            => 'Per mandar lo messatge, mercés de resoudre lo captcha ([[Special:Captcha/help|ajuda]])',
+	'contactpage-captcha-failed'     => 'Avètz pas decodat lo captcha ! ([[Special:Captcha/help|ajuda]])',
 );
---
+++ ContactPage.i18n.stq.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Internationalisation file for the ContactPage extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Daniel Kinzler, brightbyte.de
+ * @copyright © 2007 Daniel Kinzler, Betawiki users
+ * @licence GNU General Public Licence 2.0 or later
+ */
+
+/** Seeltersk (Seeltersk)
+ * @author Pyt
+ */
+$messages = array(
+	'contact'                        => 'Kontaktsiede',
+	'contactpage'                    => 'Kontaktsiede',
+	'contactpage-title'              => 'Kontakt',
+	'contactpage-pagetext'           => 'Mäd dit Formular koast du uus Ättergjuchte toukuume läite.',
+	'contactpage-defsubject'         => 'Kontakt-Ättergjucht',
+	'contactpage-subject-and-sender' => '$1 (fon $2)',
+	'contactpage-fromname'           => 'Din Noome *',
+	'contactpage-fromaddress'        => 'Dien E-Mail Adresse **',
+	'contactpage-formfootnotes'      => '* optional<br />
+** optional, is oawers nöödich, uum die oantwoudje tou konnen',
+	'contactpage-captcha'            => 'Uum ju Ättergjucht seende tou konnen, löös dät Captcha ([[Special:Captcha/help|wiedere Informatione]])',
+	'contactpage-captcha-failed'     => 'Captcha-Test nit besteen! ([[Special:Captcha/help|wiedere Informatione]])',
+);
