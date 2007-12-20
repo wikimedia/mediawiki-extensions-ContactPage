be7bef902389938e556e25d21a0b5cf36e3ccaeb
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-12-20 22:50:57 +0000

    Localisation updates from Betawiki (2007-12-20 23:25 CET). * Fixes and additions to 49 extensions for arm bg, el, fi, gl, hr, hu, loz, nl, ru, stq, sv, to

---
+++ ContactPage.i18n.bg.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Internationalisation file for the ContactPage extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Daniel Kinzler, brightbyte.de
+ * @copyright © 2007 Daniel Kinzler, Betawiki users
+ * @licence GNU General Public Licence 2.0 or later
+ */
+
+/** Bulgarian (Български)
+ * @author DCLXVI
+ */
+$messages = array(
+	'contactpage-subject-and-sender' => '$1 (от $2)',
+);
