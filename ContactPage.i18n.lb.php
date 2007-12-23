b3e797d0fd2440dad33e1d2cce11f4c2b4209c93
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-12-23 11:21:34 +0000

    Localisation updates for extensions from Betawiki (2007-12-23 11:21 CET)

---
+++ ContactPage.i18n.lb.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Internationalisation file for the ContactPage extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Daniel Kinzler, brightbyte.de
+ * @copyright © 2007 Daniel Kinzler, Betawiki users
+ * @licence GNU General Public Licence 2.0 or later
+ */
+
+/** Luxembourgish (Lëtzebuergesch)
+ * @author Robby
+ */
+$messages = array(
+	'contactpage-title'       => 'Kontakt',
+	'contactpage-fromname'    => 'Äre Numm *',
+	'contactpage-fromaddress' => 'Är E-mail-Adress **',
+);
