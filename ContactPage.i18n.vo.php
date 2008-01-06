6ac3d01f3d5864fc29d8f2d3f510112ac17e75cf
Bertrand Grondin <grondin@users.mediawiki.org>
2008-01-06 19:48:29 +0000

    Localisation updates for extensions from Betawiki (2008-01-06 19:23 UTC)

---
+++ ContactPage.i18n.vo.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Internationalisation file for the ContactPage extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Daniel Kinzler, brightbyte.de
+ * @copyright © 2007 Daniel Kinzler, Betawiki users
+ * @licence GNU General Public Licence 2.0 or later
+ */
+
+/** Volapük (Volapük)
+ * @author Malafaya
+ */
+$messages = array(
+	'contactpage-fromname' => 'nem olik *',
+);
+
