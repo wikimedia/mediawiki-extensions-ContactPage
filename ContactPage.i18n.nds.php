fd004abc37e420879ebd6b5975d95e41e8a0f94e
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-01-06 11:16:11 +0000

    Localisation updates for extension messages from Betawiki (2008-01-05 11:56 CET)

---
+++ ContactPage.i18n.nds.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Internationalisation file for the ContactPage extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Daniel Kinzler, brightbyte.de
+ * @copyright © 2007 Daniel Kinzler, Betawiki users
+ * @licence GNU General Public Licence 2.0 or later
+ */
+
+/** Low German (Plattdüütsch)
+ * @author Slomox
+ */
+$messages = array(
+	'contactpage-title'    => 'Kontakt',
+	'contactpage-fromname' => 'Dien Naam *',
+);
