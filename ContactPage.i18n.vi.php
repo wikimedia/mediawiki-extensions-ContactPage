05d2c2fe5f173c93e0b0f742bf70bef11b6ab1fa
Bertrand Grondin <grondin@users.mediawiki.org>
2008-01-30 14:12:43 +0000

    Localisation updates for extensions messages from Betawiki (2008-01-30 14:15 CET)

--- ContactPage.i18n.vi.php
+++ ContactPage.i18n.vi.php
@@ -10,6 +10,16 @@
  * @licence GNU General Public Licence 2.0 or later
  */
 
+/** Vietnamese (Tiếng Việt)
+ * @author Minh Nguyen
+ * @author לערי ריינהארט
+ */
 $messages = array(
-'contact' => 'Liên lạc',
+	'contact'                        => 'Liên lạc',
+	'contactpage-title'              => 'Liên lạc',
+	'contactpage-pagetext'           => 'Xin hãy sử dụng biểu mẫu ở dưới để liên lạc với chúng tôi.',
+	'contactpage-subject-and-sender' => '$1 (gửi từ $2)',
+	'contactpage-fromname'           => 'tên bạn *',
+	'contactpage-fromaddress'        => 'địa chỉ thư điện tử của bạn **',
 );
+
