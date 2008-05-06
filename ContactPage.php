8bfa3b1b212feb20b96fc5f71ec942dcc7e3d548
Tim Starling <tstarling@users.mediawiki.org>
2008-05-06 11:59:58 +0000

    Don't call preg_replace ~80 times per request. Leave the heavy lifting to Special:Version, using the new svn-revision member of $wgExtensionCredits.

--- ContactPage.php
+++ ContactPage.php
@@ -17,7 +17,8 @@ if( !defined( 'MEDIAWIKI' ) ) {
 
 $wgExtensionCredits['specialpage'][] = array(
 	'name' => 'ContactPage',
-	'version' => preg_replace('/^.* (\d\d\d\d-\d\d-\d\d) .*$/', '\1', '$LastChangedDate$'), #just the date of the last change
+	'svn-date' => '$LastChangedDate$',
+	'svn-revision' => '$LastChangedRevision$',
 	'author' => 'Daniel Kinzler',
 	'url' => 'http://www.mediawiki.org/wiki/Extension:ContactPage',
 	'description' => 'Contact form for visitors',
