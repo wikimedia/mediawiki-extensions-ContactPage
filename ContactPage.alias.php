981f25695fed2549036547463d36d762ea1fbcd4
Rotem Liss <rotem@users.mediawiki.org>
2008-07-10 14:07:07 +0000

    Update for he.

--- ContactPage.alias.php
+++ ContactPage.alias.php
@@ -11,12 +11,19 @@ $aliases = array();
  * @author Jon Harald Søby
  */
 $aliases['en'] = array(
-    'ContactPage'            => array('Contact', 'ContactPage')
+    'ContactPage' => array( 'Contact', 'ContactPage' ),
+);
+
+/** Hebrew (עברית)
+ * @author Rotem Liss
+ */
+$aliases['he'] = array(
+    'ContactPage' => array( 'יצירת_קשר' ),
 );
 
 /** Norwegian (Norsk (bokmål))
  * @author Jon Harald Søby
  */
 $aliases['no'] = array(
-    'ContactPage'            => array('Kontakt', 'Kontaktside')
+    'ContactPage' => array( 'Kontakt', 'Kontaktside' ),
 );
