e7a6e3b8424bb95de54f89afd6265cd6aca09849
Sam Reed <reedy@users.mediawiki.org>
2010-10-29 15:14:44 +0000

    Start removing/fixing calls to deprecated methods in WMF used extensions

--- SpecialContact.php
+++ SpecialContact.php
@@ -36,8 +36,6 @@ class SpecialContact extends SpecialPage {
 	public function execute( $par ) {
 		global $wgUser, $wgOut, $wgRequest, $wgEnableEmail, $wgContactUser;
 
-		wfLoadExtensionMessages( 'ContactPage' );
-
 		if( !$wgEnableEmail || !$wgContactUser ) {
 			$wgOut->showErrorPage( 'nosuchspecialpage', 'nospecialpagetext' );
 			return;
