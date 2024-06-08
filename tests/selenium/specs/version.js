b402ac9434aeb82f9476a55dd10c8ad5c5f87908
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2024-06-08 01:03:56 +0000

    build: Updating eslint-config-wikimedia to 0.28.0
    Change-Id: I2f1ac4e3fec1dcba6050bde5c0020a3e729fd97c

--- tests/selenium/specs/version.js
+++ tests/selenium/specs/version.js
@@ -3,8 +3,8 @@
 const assert = require( 'assert' ),
 	VersionPage = require( '../pageobjects/version.page' );
 
-describe( 'ContactPage on Version page', function () {
-	it( 'ContactPage is listed in the version page under the special page category', async function () {
+describe( 'ContactPage on Version page', () => {
+	it( 'ContactPage is listed in the version page under the special page category', async () => {
 		await VersionPage.open();
 
 		assert( await VersionPage.contactPageExtension.isExisting() );
