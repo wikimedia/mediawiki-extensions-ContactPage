2f58b313acd2cb25f38709b23abe2ea7943cda52
Željko Filipin <zeljko.filipin@gmail.com>
2024-10-09 13:41:06 +0200

    selenium: Replace Node.js Assert library with WebdriverIO Expect library
    Assertions from Expect library are more readable that assertions from Assert library.
    
    Bug: T325740
    Change-Id: Ib5bb2ab53bf322654b860fb5126b198abc94cf25

--- tests/selenium/specs/version.js
+++ tests/selenium/specs/version.js
@@ -1,12 +1,11 @@
 'use strict';
 
-const assert = require( 'assert' ),
-	VersionPage = require( '../pageobjects/version.page' );
+const VersionPage = require( '../pageobjects/version.page' );
 
 describe( 'ContactPage on Version page', () => {
 	it( 'ContactPage is listed in the version page under the special page category', async () => {
 		await VersionPage.open();
 
-		assert( await VersionPage.contactPageExtension.isExisting() );
+		await expect( VersionPage.contactPageExtension ).toExist();
 	} );
 } );
